c659829f27a4763fcd0f3c55d4633f6067362168
Raunak Kumari <kumariraunak1920@gmail.com>
2026-07-18 19:57:36 +0530

    Fix CORS for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -29,8 +29,10 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+origin: [
+  "http://localhost:5173",
+  "https://smart-matrimonial-platform-u3hk-6op065mtx.vercel.app",
+],    methods: ["GET", "POST"],
     credentials: true,
   },
 });
@@ -43,7 +45,10 @@ app.set("io", io);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://smart-matrimonial-platform-u3hk-6op065mtx.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
