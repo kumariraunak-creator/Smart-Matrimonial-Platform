508c8f3e84f5668853b774d468d3b5b2d196d079
Raunak Kumari <kumariraunak1920@gmail.com>
2026-07-18 20:36:05 +0530

    Allow all origins temporarily

--- server/server.js
+++ server/server.js
@@ -29,31 +29,17 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
   cors: {
-origin: [
-  "http://localhost:5173",
-  "https://smart-matrimonial-platform-u3hk-6op065mtx.vercel.app",
-],    methods: ["GET", "POST"],
+    origin: true,
     credentials: true,
   },
 });
-
 app.set("io", io);
 
 /* ===========================
             CORS
 =========================== */
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://smart-matrimonial-platform-u3hk-6op065mtx.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
 
 /* ===========================
         MIDDLEWARE
